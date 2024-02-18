02ded578d44b08a552bd2a50098d0023d6c55422
AMomozZz <malongjordanl@126.com>
2024-02-18 22:41:48 +0100

    edge to graph code I used in ID2222

---
+++ ConvertFile.py
@@ -0,0 +1,21 @@
+import networkx as nx
+
+class ConvertFile:
+        """
+        A class providing methods to convert a file to a NetworkX graph.
+        """
+        
+        @staticmethod
+        def toGraph(filePath, d, weight = False):
+                """
+                Converts a file into a NetworkX graph.
+
+                Args:
+                - filePath (str): The path to the input file.
+                - d (str): The delimiter used in the file.
+                - weight (bool): Determines if the graph is weighted (default: False).
+
+                Returns:
+                - graph (networkx.Graph): The NetworkX graph converted from the file.
+                """
+                return nx.read_weighted_edgelist(filePath, delimiter = d) if weight else nx.read_edgelist(filePath, delimiter = d)

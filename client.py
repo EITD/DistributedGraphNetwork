56186c056ff79e38287fd3eb1ae349c729a7f1cc
EITD <islahou2022@gmail.com>
2024-02-19 22:09:47 +0100

    change to mysocket

--- client.py
+++ client.py
@@ -1,21 +1,32 @@
 import socket
 import json
+from MySocket import MySocket
 
-def query_node_feature(port, nid, k, deltas):
+def query_node_feature():
     request_data = {
-        'nid': nid,
-        'k': k,
-        'deltas': deltas
+        'node_feature': "1"
     }
-
     request_json = json.dumps(request_data)
+    s = MySocket(myNode=None, port=10000, NUM_PARTITIONS=4, client=True)
+    s.ask(0, node=1, msg=request_json)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(('localhost', port))
-        s.sendall(request_json.encode())
-        response = s.recv(1024)
-        print(f"Response: {response.decode()}")
+query_node_feature()
 
-# Example query
-# This should match the port of the worker responsible for the node being queried
-query_node_feature(10000, '0', 2, [2, 2])  # Querying worker 0 for node 1's feature
+# def query_node_feature(port, nid, k, deltas):
+#     request_data = {
+#         'nid': nid,
+#         'k': k,
+#         'deltas': deltas
+#     }
+
+#     request_json = json.dumps(request_data)
+
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#         s.connect(('localhost', port))
+#         s.sendall(request_json.encode())
+#         response = s.recv(1024)
+#         print(f"Response: {response.decode()}")
+
+# # Example query
+# # This should match the port of the worker responsible for the node being queried
+# query_node_feature(10000, '0', 2, [2, 2])  # Querying worker 0 for node 1's feature
--- worker.py
+++ worker.py
@@ -2,21 +2,23 @@ import socket
 from multiprocessing import Process
 from ConvertFile import ConvertFile, nx
 import json
+from MySocket import MySocket
+import sys
 
-NUM_PARTITIONS_START = 0
-NUM_PARTITIONS_END = 4
-PARTITION_LIST = [i for i in range(NUM_PARTITIONS_START, NUM_PARTITIONS_END)]
+# NUM_PARTITIONS_START = 0
+# NUM_PARTITIONS_END = 4
+# PARTITION_LIST = [i for i in range(NUM_PARTITIONS_START, NUM_PARTITIONS_END)]
 NODE_FEATURES = "./data/node_features.txt"
 
 class Worker:
     worker_id = None
-    port = None
+    s = None
     node_data = {}
     graph = {}
 
-    def __init__(self, wid, port):
+    def __init__(self, wid, p):
         self.worker_id = wid
-        self.port = port
+        self.s = MySocket(myNode=wid, port=p, NUM_PARTITIONS=4)
 
     def load_node_data(self):
         with open(NODE_FEATURES, 'r') as file:
@@ -29,58 +31,70 @@ class Worker:
     def load_graph_dict(self):
         self.graph = ConvertFile.toGraph(f"./data/partition_{self.worker_id}.txt", " ")
 
-    def khop_neighborhood(self, nid, k, deltas):
-        if nid in self.node_data:
-            features_sum = self.node_data[nid]
-            if k == 0 or not deltas:
-                return features_sum
+    def start_listen(self):
+        client_socket, message = self.s.message_get_queue.get()
+        request_data = json.loads(message)
+        if 'node_feature' in request_data:
+            print("here1")
+            nid = request_data['node_feature']
+            if nid in self.node_data:
+                print("here2")
+                self.s.message_send_queue.put((client_socket, self.node_data[nid]))
+            else:
+                print("here3")
+                self.s.ask(0, node=int(nid), msg=message)
+
+    
+    # def khop_neighborhood(self, nid, k, deltas, num):
+    #     if nid in self.node_data:
+    #         features_sum = self.node_data[nid]
+    #         if k == 0 or not deltas:
+    #             return features_sum
             
-            count = 0
-            for neighbor in list(self.graph.neighbors(nid)):
-                print("send to others")
-                # features_sum += khop_neighborhood(neighbor, k - 1, deltas[1:])
-                count += 1
-                if count == deltas[0]:
-                    break
+    #         count = 0
+    #         for neighbor in list(self.graph.neighbors(nid)):
+    #             if len(deltas) > 1:
 
-            return features_sum
+    #             print("send to others")
+    #             # features_sum += khop_neighborhood(neighbor, k - 1, deltas[1:])
+    #             # return -1 continue 
+    #             count += 1
+    #             if count == deltas[0]:
+    #                 break
+            
+    #         if count < deltas[0]:
+    #             return -1
+
+    #         return features_sum
         
-        else:
-            print("send to others")
+    #     else:
+    #         print("send to others")
         
 
-    def start_worker(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('localhost', self.port))
-            s.listen()
-            print(f"Worker {self.worker_id} listening on port {self.port}")
-            while True:
-                conn, addr = s.accept()
-                with conn:
-                    data = conn.recv(1024).decode()
-                    request_data = json.loads(data)
-                    nid = request_data['nid']
-                    k = request_data['k']
-                    deltas = request_data['deltas']
-                    result = self.khop_neighborhood(nid, k, deltas)
-                    conn.sendall(str(result).encode())
+    # def start_worker(self):
+    #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #         s.bind(('localhost', self.port))
+    #         s.listen()
+    #         print(f"Worker {self.worker_id} listening on port {self.port}")
+    #         while True:
+    #             conn, addr = s.accept()
+    #             with conn:
+    #                 data = conn.recv(1024).decode()
+    #                 request_data = json.loads(data)
+    #                 nid = request_data['nid']
+    #                 k = request_data['k']
+    #                 deltas = request_data['deltas']
+    #                 result = self.khop_neighborhood(nid, k, deltas)
+    #                 conn.sendall(str(result).encode())
     
 
 def start_worker(wid, port):
     worker = Worker(wid, port)
     worker.load_node_data()
     worker.load_graph_dict()
-    worker.start_worker()
-
+    while(True):
+        worker.start_listen()
 
 if __name__ == "__main__":
-    # workersDict = {}
-    # graphDict = {}
-
-    for i in PARTITION_LIST:
-        w = Process(target=start_worker, args=(i, 10000+i))
-        # g = ConvertFile.toGraph(f"./data/partition_{i}.txt", " ")
-        # workersDict[i] = w
-        # graphDict[i] = g
+        w = Process(target=start_worker, args=(sys.argv[1], 12345+int(sys.argv[1])))
         w.start()
-        # w.join()

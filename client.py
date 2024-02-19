95fa6863c63c683240169b08a5d9991c80b1ebbe
EITD <islahou2022@gmail.com>
2024-02-19 02:40:21 +0100

    Neighbourhood queries for internal nodes

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
 import socket
+import json
 
-def query_node_feature(port, nid):
+def query_node_feature(port, nid, k, deltas):
+    request_data = {
+        'nid': nid,
+        'k': k,
+        'deltas': deltas
+    }
+
+    request_json = json.dumps(request_data)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(('localhost', port))
-        s.sendall(nid.encode())
-        feature = s.recv(1024)
-        print(f"Node {nid} feature: {feature.decode()}")
+        s.sendall(request_json.encode())
+        response = s.recv(1024)
+        print(f"Response: {response.decode()}")
 
 # Example query
 # This should match the port of the worker responsible for the node being queried
-query_node_feature(10000, '16')  # Querying worker 0 for node 1's feature
+query_node_feature(10000, '0', 2, [2, 2])  # Querying worker 0 for node 1's feature
--- worker.py
+++ worker.py
@@ -1,47 +1,102 @@
 import socket
 from multiprocessing import Process
 from ConvertFile import ConvertFile, nx
-from matplotlib import pyplot as plt
+import json
 
 NUM_PARTITIONS_START = 0
 NUM_PARTITIONS_END = 4
 PARTITION_LIST = [i for i in range(NUM_PARTITIONS_START, NUM_PARTITIONS_END)]
 NODE_FEATURES = "./data/node_features.txt"
 
-def load_node_data(worker_id):
+class Worker:
+    worker_id = None
+    port = None
     node_data = {}
-    with open(NODE_FEATURES, 'r') as file:
-        lines = file.readlines()
-    for line in lines:
-        parts = line.strip().split()
-        if int(parts[0]) % 4 == worker_id and len(parts) == 2:
-            node_data[parts[0]] = parts[1]
-    return node_data
+    graph = {}
 
-def worker(worker_id, port, node_data):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('localhost', port))
-        s.listen()
-        print(f"Worker {worker_id} listening on port {port}")
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                nid = conn.recv(1024).decode()
-                feature = node_data.get(nid, "Node not found")
-                conn.sendall(str(feature).encode())
+    def __init__(self, wid, port):
+        self.worker_id = wid
+        self.port = port
 
-def start_worker(worker_id, port):
-    worker(worker_id, port, load_node_data(worker_id))
+    def load_node_data(self):
+        with open(NODE_FEATURES, 'r') as file:
+            lines = file.readlines()
+        for line in lines:
+            parts = line.strip().split()
+            if int(parts[0]) % 4 == self.worker_id and len(parts) == 2:
+                self.node_data[parts[0]] = parts[1]
+
+    def load_graph_dict(self):
+        self.graph = ConvertFile.toGraph(f"./data/partition_{self.worker_id}.txt", " ")
+
+    def khop_neighborhood(self, nid, k, deltas):
+        if k == 0 or not deltas:
+            return self.node_data(nid)
+
+        current_layer_nodes = {nid}
+        features_sum = 0
+        
+        for deltas_index in range(0, k):
+            total_nodes_needed = deltas[deltas_index]
+            next_layer_nodes = set()
+            
+            for node in current_layer_nodes:
+                neighbors = list(self.graph.neighbors(node))
+                for neighbor in neighbors:
+                    next_layer_nodes.add(neighbor)
+                    if len(next_layer_nodes) == total_nodes_needed:
+                        break  
+                if len(next_layer_nodes) == total_nodes_needed:
+                    break
+            
+            if len(next_layer_nodes) < total_nodes_needed:
+                print("not enough")
+
+            for node in current_layer_nodes:
+                features_sum += int(self.node_data[node])
+            # print(current_layer_nodes)
+        
+            current_layer_nodes = next_layer_nodes.copy()
+        
+        for node in current_layer_nodes:
+            features_sum += int(self.node_data[node])
+        # print(current_layer_nodes)
+
+        return features_sum
+        
+
+    def start_worker(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('localhost', self.port))
+            s.listen()
+            print(f"Worker {self.worker_id} listening on port {self.port}")
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    data = conn.recv(1024).decode()
+                    request_data = json.loads(data)
+                    nid = request_data['nid']
+                    k = request_data['k']
+                    deltas = request_data['deltas']
+                    result = self.khop_neighborhood(nid, k, deltas)
+                    conn.sendall(str(result).encode())
+    
+
+def start_worker(wid, port):
+    worker = Worker(wid, port)
+    worker.load_node_data()
+    worker.load_graph_dict()
+    worker.start_worker()
+
 
 if __name__ == "__main__":
-    workersDict = {}
-    graphDict = {}
+    # workersDict = {}
+    # graphDict = {}
 
     for i in PARTITION_LIST:
         w = Process(target=start_worker, args=(i, 10000+i))
-        g = ConvertFile.toGraph(f"./data/partition_{i}.txt", " ")
-        workersDict[i] = w
-        graphDict[i] = g
+        # g = ConvertFile.toGraph(f"./data/partition_{i}.txt", " ")
+        # workersDict[i] = w
+        # graphDict[i] = g
         w.start()
-    # for w in workers:
-    #     w.join()
+        # w.join()

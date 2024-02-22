74962797a35084d3cf5dcdaa3952dd8a648c8035
AMomozZz <malongjordanl@126.com>
2024-02-22 20:41:41 +0100

    update client test

--- client.py
+++ client.py
@@ -1,30 +1,35 @@
+import signal
 import socket
 import json
+import sys
 from MySocket import MySocket
 
-def query_node_feature():
+s = MySocket(myNode=None, port=10000, NUM_PARTITIONS=4, client=True)
+
+def query_node_feature(nid):
     request_data = {
-        'node_feature': "1"
+        'node_feature': str(nid)
     }
     request_json = json.dumps(request_data)
-    s = MySocket(myNode=None, port=10000, NUM_PARTITIONS=4, client=True)
-    s.ask(0, node=1, msg=request_json)
-
-query_node_feature()
-
+    s.ask(0, node=nid, msg=request_json)
 
 def query_khop_neighborhood(nid, k, deltas):
+    if type(deltas) is int:
+        deltas = [deltas]
     request_data = {
         'khop_neighborhood': {
-            'nid': nid,
+            'nid': str(nid),
             'k': k,
             'deltas': deltas
         }
     }
-
     request_json = json.dumps(request_data)
-
-    s = MySocket(myNode=None, port=10000, NUM_PARTITIONS=4, client=True)
     s.ask(0, node=nid, msg=request_json)
 
-query_khop_neighborhood('0', 2, [2, 2])  
+# query_node_feature(0)
+
+# query_node_feature(1)
+
+query_khop_neighborhood(3, 1, 5)
+
+query_khop_neighborhood(3, 3, [2, 18, 32])

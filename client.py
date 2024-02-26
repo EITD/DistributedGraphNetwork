057c4d15fce22a3c9987f18a4086441bcd3ba3e9
AMomozZz <malongjordanl@126.com>
2024-02-26 21:07:28 +0100

    not use json

--- client.py
+++ client.py
@@ -7,33 +7,35 @@ from MySocket import MySocket
 s = MySocket(myNode=None, port=10000, NUM_PARTITIONS=4, client=True)
 
 def query_node_feature(nid):
-    request_data = {
-        'node_feature': str(nid)
-    }
-    request_json = json.dumps(request_data)
-    s.ask(0, node=nid, msg=request_json)
+    # request_data = {
+    #     'node_feature': {
+    #         nid: str(nid)
+    #     }
+    # }
+    # request_json = json.dumps(request_data)
+    s.ask(0, nid, str(('node_feature', str(nid))))
 
 def query_khop_neighborhood(nid, k, deltas):
     if type(deltas) is int:
         deltas = [deltas]
-    request_data = {
-        'khop_neighborhood': {
-            'nid': str(nid),
-            'k': k,
-            'deltas': deltas
-        }
-    }
-    request_json = json.dumps(request_data)
-    s.ask(0, node=nid, msg=request_json)
+    # request_data = {
+    #     'khop_neighborhood': {
+    #         'nid': str(nid),
+    #         'k': k,
+    #         'deltas': deltas
+    #     }
+    # }
+    # request_json = json.dumps(request_data)
+    s.ask(0, nid, str(('khop_neighborhood', str(nid), k, deltas)))
 
 def aggregate_neighborhood(nid, epochs):
-    request_data = {
-        'neighborhood_aggregation': {
-            'epochs': epochs
-        }
-    }
-    request_json = json.dumps(request_data)
-    s.ask(0, node=nid, msg=request_json)
+    # request_data = {
+    #     'neighborhood_aggregation': {
+    #         'epochs': epochs
+    #     }
+    # }
+    # request_json = json.dumps(request_data)
+    s.ask(0, nid, str(('neighborhood_aggregation', epochs)))
 
 
 # query_node_feature(0)

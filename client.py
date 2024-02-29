cd8be06ba4950178d4fbb9532c58e92cf1227884
AMomozZz <malongjordanl@126.com>
2024-02-29 16:21:31 +0100

    change test, can test train when k > 1

--- client.py
+++ client.py
@@ -5,6 +5,9 @@ import xmlrpc.client
 import threading
 from ConvertFile import ConvertFile
 
+# graph all edges from 4 partition
+all_graph = ConvertFile.toGraph(f"./data/neighbor.txt", " ")
+
 def send_message(message):
     start_time = time.time()
     with xmlrpc.client.ServerProxy("http://localhost:12345") as proxy:
@@ -26,16 +29,22 @@ def test(response):
         #                 print('False at:', key, 'get:', value, 'should be:', 2 ** epoch)
     
     # when node feature all 1(load dummmy), default is 1, 1 epoch all neighbors
-        all_graph = ConvertFile.toGraph(f"./data/neighbor.txt", " ")
         data = json.loads(response)['epoch_dict']
+        k = 2
         for key, value in data.items():
-                try:
-                    v = len(list(all_graph.neighbors(key)))
-                    if (v + 1) != value:
-                        # if v != 0:
-                            print("Warning:", key, value, 'should be:', v + 1)
-                except (networkx.exception.NetworkXError):
-                    pass
+            sums = 1
+            neighborsList = list(all_graph.neighbors(key))
+            sums += len(neighborsList)
+            sums += recursion(k - 1, neighborsList)
+            if sums != value:
+                print("Warning:", key, 'value:', value, 'should be:', sums)
+
+def recursion(k, neighborsList):
+    for n in neighborsList:
+        temp = list(all_graph.neighbors(n))
+        sums += len(neighborsList)
+        if k == 1: return sums
+        else: recursion(k-1, temp)
 
 def query_node_feature(nid):
     request_data = {

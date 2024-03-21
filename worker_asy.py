1f3dc332d12551f2b233e7612b2766251bef46ef
EITD <islahou2022@gmail.com>
2024-03-21 11:25:46 +0100

    comment random error

--- worker_asy.py
+++ worker_asy.py
@@ -403,11 +403,11 @@ def notify(node, msg, worker=False):
         client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
         client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         try:
-            r = random.randint(0,2)
-            if r == 1:
-                raise ConnectionRefusedError()
-            elif r == 2:
-                raise OSError()
+            # r = random.randint(0,2)
+            # if r == 1:
+            #     raise ConnectionRefusedError()
+            # elif r == 2:
+            #     raise OSError()
             
             if worker:
                 client_socket.connect((serverDict[int(node) % NUM_PARTITIONS], 10000 + int(node)))
